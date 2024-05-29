fb1bc0eba1642437cf7ba8bdba1e78fe0f316acc
Uditya Kumar <shubhamkumaar2001@gmail.com>
2024-05-30 01:34:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-//complete this code
-class Person {}
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Setter for age
+  set age(newAge) {
+    this._age = newAge;
+  }
+
+  // Getter for age (optional if you want to log age)
+  get age() {
+    return this._age;
+  }
+}
+
+class Student extends Person {
+  study() {
+    console.log(`${this._name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+  teach() {
+    console.log(`${this._name} is teaching`);
+  }
+}
+
 
 // Do not change the code below this line
 window.Person = Person;
